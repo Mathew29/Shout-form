794926887a0c1f3bd1aef9d8c31371254e94fe51
Brian Hensley and Mathew Akre <brian@>
2019-03-27 13:05:33 -0700

    add font size to output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
   $("#shout-form").submit(function(event) {
     var saidInput = $("input#said").val();
-    // var output = saidInput.toUpperCase();
 
-    $(".said").text(output);
+    $(".said").text(saidInput.toUpperCase());
 
     $("#shout").show();
     event.preventDefault();
